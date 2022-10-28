73bc9dc6c93db8a3e5196b77e82db3254da5459f
The-Subpar-Scholar <fizzpope@gmail.com>
2022-10-27 17:54:10 -0700

    Changed the method of aquiring the file directory

--- RankWords.py
+++ RankWords.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
+from os import path
+TEXT_FILE_NAME = 'text.txt'
+
 word_dict = defaultdict(int)
-path_name = r"/Users/fuuswik/Python Files/Rank Words/text.txt"
+path_name = path.dirname(path.abspath(__file__)) + '\\' + TEXT_FILE_NAME # path to directory with file name at end
 make_red = lambda s: f"\033[1;31;49m{s}\033[0;37;49m"
 make_blue = lambda s: f"\033[1;34;49m{s}\033[0;37;49m"
 
